8fbce2a45053c638ad4255e59775d95ae2f0589c
VrishabhA <vrishabh.alva@gmail.com>
2020-08-12 19:09:14 +0530

    Added all routes

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ let express         = require("express"),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
     Playlist 		= require("./models/playlist")
-    // methodOverride  = require("method-override"),
+    methodOverride  = require("method-override"),
     app = express();
 
     app.use(express.static(__dirname + "/public")); 
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(methodOverride("_method"));
     app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/playlists", { useNewUrlParser: true , useUnifiedTopology:true});
@@ -32,6 +33,69 @@ app.get("/playlists/:id/", function(req,res){
 	})
 });
 
+
+//New 
+app.get("/newpl",function(req,res){
+	res.render("new");
+});
+
+app.post("/playlists",function(req,res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var URL = req.body.URL;
+	var eUrl = req.body.eUrl;
+
+	var newPL = new Playlist({
+		name 		: name,
+		image	   	: image,
+		description	: description,
+		URL 		: URL,
+		eUrl		: eUrl
+	});
+
+	Playlist.create(newPL, function(err, PL){
+		if (err) {
+			console.log(err)
+		}else{
+
+			console.log("Playlist Added!");
+			res.redirect("/playlists");
+		}
+	});
+});
+
+//Edit
+
+app.get("/playlists/:id/edit", function(req,res){
+	Playlist.findById(req.params.id, function(err, playlist){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("edit",{playlist: playlist});
+		}
+	});
+});
+
+app.put("/playlists/:id",function(req,res){
+    Playlist.findOneAndUpdate({_id: req.params.id}, req.body.playlist, {useFindAndModify: false}).exec(function(err,updatedPlaylist){
+	    if(err){
+	        console.log(err);
+	    } else{
+	        res.redirect("/playlists/"+req.params.id);
+	    }
+	});
+});
+
+app.delete("/playlists/:id", function(req,res){
+	Playlist.findOneAndDelete({_id: req.params.id}).exec(function(err,deletedPlaylist){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/playlists");
+		}
+	})
+});
 // var newPL = new Playlist({
 // 	name: "FAYAH",
 // 	image: "https://www.melodynest.com/wp-content/uploads/2019/06/SPACE-BLANK_chs060619-607x607.png",
