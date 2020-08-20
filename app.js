3c253b5085a6a9afedff7ee2aaeddf5b678df66e
VrishabhA <vrishabh.alva@gmail.com>
2020-08-20 17:38:34 +0530

    Started Adding Flash Messages

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let express         = require("express"),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
-    methodOverride  = require("method-override"),
+    flash			= require("connect-flash"),
     passport        = require("passport"),
     LocalStrategy   = require("passport-local"),
-
+    methodOverride  = require("method-override"),
     User 			= require("./models/user"),
     Playlist 		= require("./models/playlist"),
     app = express();
@@ -17,6 +17,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
+app.use(flash());
 
 app.use(require("express-session")({
     secret:"John Cena",
@@ -28,6 +29,7 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,15 @@ router.get("/", function(req,res){
 
 // Auth Routes
 router.get("/register",function(req,res){
-	res.render("register");
+	res.render("register",{message: false});
 });
 
 router.post("/register",function(req,res){
 	var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password,function(err,user){
         if(err){
-            console.log(err);
-            res.render("register");
+            req.flash("error",err.message);
+            res.render("register",{message: req.flash("error")});
         }
         passport.authenticate("local")(req,res,function(){
             res.redirect("/playlists");
@@ -35,6 +35,7 @@ router.get("/login", function(req, res){
 router.post("/login", passport.authenticate("local",{
     successRedirect:"/playlists",
     failureRedirect: "/login",
+    failureFlash : true
 }), function(req,res,next){
 
 });
