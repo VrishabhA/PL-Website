09e31488626885e5174829765a47b53c615465bc
unknown <vrishabh.alva@gmail.com>
2020-09-02 13:13:12 +0530

    test

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
-    res.locals.message = req.flash("error");
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    res.locals.warning = req.flash("warning");
     next();
 });
 
