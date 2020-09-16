9bf2f8f51d8f78630dd15d7586910d7000623bc7
unknown <vrishabh.alva@gmail.com>
2020-09-16 20:26:53 +0530

    Added MomentJs and initiated handling no comments

--- routes/playlist.js
+++ routes/playlist.js
@@ -23,6 +23,14 @@ router.get("/:id", function(req,res){
 		if(err){
 			req.flash("error",err);
 		}else{
+
+			console.log(playlist)
+			/*var flag=1;
+			playlist.comments.count({}, function(error, num) {
+				if(num==0){
+					flag = 0;
+				}
+			});*/
 			res.render("playlist/show",{playlist: playlist});
 		}
 	})
